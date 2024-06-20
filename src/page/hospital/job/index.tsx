87ceb68f4ac3564fa707a34b2b75ad85f885aa68
RUGINA <michel.igiraneza@auca.ac.rw>
2024-06-20 17:54:23 +0200

    HospitalJobs

---
+++ src/page/hospital/job/index.tsx
@@ -0,0 +1,8 @@
+import { Navigation } from "../../../component/navigation"
+import { HospitalMenu } from "../../../utils/hospitalMenu"
+
+export const HospitalJobs=()=>{
+    return <Navigation navItems={HospitalMenu}>
+        <div></div>
+    </Navigation>
+}
