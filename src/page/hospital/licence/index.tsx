53b90d4f364cf79997a88d33bd65bb2caecc72ee
RUGINA <michel.igiraneza@auca.ac.rw>
2024-06-20 17:54:35 +0200

    Hospital Licence

---
+++ src/page/hospital/licence/index.tsx
@@ -0,0 +1,8 @@
+import { Navigation } from "../../../component/navigation"
+import { HospitalMenu } from "../../../utils/hospitalMenu"
+
+export const HospitalLicence=()=>{
+    return <Navigation navItems={HospitalMenu}>
+        <div></div>
+    </Navigation>
+}
