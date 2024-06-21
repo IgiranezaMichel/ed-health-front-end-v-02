54dc172838daf3ed9fa58bd52858ee45d61a249d
RUGINA <michel.igiraneza@auca.ac.rw>
2024-06-21 14:00:37 +0200

    School Admin

---
+++ src/page/school/index.tsx
@@ -0,0 +1,9 @@
+import { Navigation } from "../../component/navigation"
+import { SchoolMenu } from "../../utils/schoolMenu"
+export const SchoolAdmin=()=>{
+    return <Navigation navItems={SchoolMenu}>
+        <div>
+            
+        </div>
+    </Navigation>
+}
