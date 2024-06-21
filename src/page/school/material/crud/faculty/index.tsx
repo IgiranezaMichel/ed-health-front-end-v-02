5491baabef207a8ad4bb082bfb409611b4618025
RUGINA <michel.igiraneza@auca.ac.rw>
2024-06-21 14:06:06 +0200

    SchoolFacultyIndex

---
+++ src/page/school/material/crud/faculty/index.tsx
@@ -0,0 +1,23 @@
+import { Box, Button, Dialog, IconButton } from "@mui/material"
+import { DisplayFaculty } from "./display"
+import { AddCircle, Close } from "@mui/icons-material"
+import { CreateFaculty } from "./create"
+import { useState } from "react"
+
+export const SchoolFacultyIndex=()=>{
+    const[newFaculty,setNewFaculty]=useState(false);
+    return<>
+    <div className="d-flex justify-content-between align-items-center">
+        <div className="fw-bolder">Faculty table</div>
+        <Button variant="outlined" onClick={()=>setNewFaculty(true)}> <AddCircle/> create</Button>
+    </div>
+    <DisplayFaculty/>
+    <Dialog open={newFaculty} maxWidth='xs' PaperProps={{className:'col-12'}}>
+        <CreateFaculty>
+            <Box className='d-flex justify-content-between p-2 align-items-center sticky-top bg-white'>
+                <div>New Faculty</div> <IconButton onClick={()=>setNewFaculty(false)}><Close/></IconButton>
+            </Box>
+        </CreateFaculty>
+    </Dialog>
+    </>
+}
