f41c87bc06e0148a7ad8974c5b6e6577ec1de524
RUGINA <michel.igiraneza@auca.ac.rw>
2024-06-30 13:12:26 +0200

    StudentJob

---
+++ src/page/student/job/index.tsx
@@ -0,0 +1,51 @@
+import { Navigation } from "../../../component/navigation"
+import { StudentMenu } from "../../../utils/studentMenu"
+import { Chip, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+
+export const StudentJob=()=>{
+    return <Navigation navItems={StudentMenu} >
+         <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+                <TableCell >
+                 Table
+                </TableCell>
+                <TableCell >
+                 Table
+                </TableCell>
+                <TableCell >
+                 Table
+                </TableCell>
+                <TableCell >
+                 Table
+                </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+                  <TableRow hover role="checkbox" tabIndex={-1} >
+                        <TableCell>
+                           Mich  
+                        </TableCell>
+                        <TableCell >
+                           Mich  
+                        </TableCell>
+                        <TableCell >
+                           Mich  
+                        </TableCell>
+                        <TableCell >
+                           Mich 
+                        </TableCell>
+                  </TableRow>
+            
+              <TableRow>
+                <TableCell colSpan={4} className="text-center"><Chip label='No data found'/></TableCell>
+                </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+       
+    </Paper>
+    </Navigation>
+}
