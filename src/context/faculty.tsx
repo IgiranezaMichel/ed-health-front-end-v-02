6192e28a1147ad43b21271241082507ca7b5ce04
RUGINA <michel.igiraneza@auca.ac.rw>
2024-07-02 15:22:50 +0200

    Faculty Provider

--- src/context/faculty.tsx
+++ src/context/faculty.tsx
@@ -1,9 +1,29 @@
-import { createContext, useContext } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import { IRefresh } from "../interface/refresh";
+import { FacultyDao } from "../controller/faculty";
 
-export const FacultyRefreshContext=createContext<IRefresh|undefined>(undefined);
+const FacultyContext=createContext<IRefresh|undefined>(undefined);
 export const useFacultyContext=()=>{
-const location=useContext(FacultyRefreshContext);
+const location=useContext(FacultyContext);
 if(!location)throw Error('faculty  is not in context')
 return location;
+}
+
+export const FacultyProvider=(prop:{children:ReactNode})=>{
+    const [facultyList,setFacultList]=useState([]);
+    const [refresh,setRefresh]=useState(true);
+    const facultyDao=new FacultyDao().getAllFacultyFromSchool();
+    useEffect(
+      ()=>{
+        facultyDao.then(data=>{setFacultList(data.data);setRefresh(false)});
+        
+      },[refresh]
+  )
+    const data:IRefresh={
+         data:facultyList,
+         refresh:()=>setRefresh(!refresh)
+    }
+    return <FacultyContext.Provider value={data}>
+    {prop.children}
+    </FacultyContext.Provider>
 }
