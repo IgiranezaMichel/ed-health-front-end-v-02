5ec4c8a8509fb4464de80facb1fcc518746e07c0
RUGINA <michel.igiraneza@auca.ac.rw>
2024-07-02 15:24:36 +0200

    refactor getSchoolDepartments parameters

--- src/controller/department.tsx
+++ src/controller/department.tsx
@@ -3,16 +3,14 @@ import { IDepartment } from "../interface/entity/department";
 
 const API='http://localhost:8080/api/department';
 export class DepartmentDao{
-    private schoolId:string='3bd1351c-7a7d-4c55-989a-63efdae28275'
     public saveDepartment=async(department:IDepartment)=>{
         return await axios.post(API+'/create',department,{withCredentials:true});
     }
     public updateDepartment=async(department:IDepartment)=>{
         return await axios.post(API+'/update',department,{withCredentials:true});
     }
-    public getSchoolDepartments=async(schoolId:string)=>{
-        if(schoolId.length==0)schoolId=this.schoolId
-        return await axios.get(API+'/all/'+schoolId,{withCredentials:true});
+    public getSchoolDepartments=async()=>{
+        return await axios.get(API+'/all',{withCredentials:true});
     }
     public deleteDepartment=async(departmentId:string)=>{
         return await axios.delete(API+'/'+departmentId,{withCredentials:true});
