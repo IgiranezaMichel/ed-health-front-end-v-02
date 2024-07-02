369040045def9aedbef462cc843724014ca3a69d
RUGINA <michel.igiraneza@auca.ac.rw>
2024-07-02 15:22:39 +0200

    create department

--- src/context/department.tsx
+++ src/context/department.tsx
@@ -1,9 +1,29 @@
-import { createContext, useContext } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import { IRefresh } from "../interface/refresh";
+import { DepartmentDao } from "../controller/department";
 
-export const DepartmentRefreshContext=createContext<IRefresh|undefined>(undefined);
+ const DepartmentContext=createContext<IRefresh|undefined>(undefined);
 export const useDepartmentContext=()=>{
-const location=useContext(DepartmentRefreshContext);
+const location=useContext(DepartmentContext);
 if(!location)throw Error('Department  is not in context')
 return location;
+}
+
+export const DepartmentProvider=(prop:{children:ReactNode})=>{
+    const [departmentList,setDepartmentList]=useState([]);
+    const [refresh,setRefresh]=useState(true);
+    const departmentDao=new DepartmentDao().getSchoolDepartments();
+    useEffect(
+      ()=>{
+        departmentDao.then(data=>{setDepartmentList(data.data);setRefresh(false)});
+        
+      },[refresh]
+  )
+    const data:IRefresh={
+         data:departmentList,
+         refresh:()=>setRefresh(!refresh)
+    }
+    return <DepartmentContext.Provider value={data}>
+    {prop.children}
+    </DepartmentContext.Provider>
 }
