5895d9f964c3f2f033a3958314c2472d6849facb
RUGINA <michel.igiraneza@auca.ac.rw>
2024-07-02 15:23:48 +0200

    Student Provider

--- src/context/student.tsx
+++ src/context/student.tsx
@@ -2,25 +2,25 @@ import { ReactNode, createContext, useContext, useEffect, useState } from "react
 import { IState } from "../interface/stateContext";
 import { StudentDao } from "../controller/student";
 import { IPage } from "../interface/page";
+import { StudentStatus } from "../enum/StudentStatus";
 export const StudentContext=createContext<IState|undefined>(undefined);
 export const useStudentContext=()=>{
 const location=useContext(StudentContext);
 if(!location)throw Error('location is undefined')
 return location;
 }
-export const StudentProvider=(prop:{children:ReactNode})=>{
+export const StudentProvider=(prop:{children:ReactNode,status:StudentStatus})=>{
     const [refresh,setRefresh]=useState(false);
      const [page,setPage]=useState<IPage>({pageNumber:0,pageSize:10,search:'',sort:'id'});
-    const user=JSON.parse(String(localStorage.getItem('user')));
     const [studentList,setStudentList]=useState<any>([]);
-    const studentDao=new StudentDao().getAllStudentFromSameSchool(user!=null?user.referenceId:'',page);
+    const studentDao=new StudentDao().getAllStudentFromSameSchool(page,prop.status);
     useEffect(
         ()=>{
          studentDao.then(data=>{{
             console.log(data.request.readyState);
             
             setStudentList(data.data);}}).catch(err=>console.log(err))
-        },[page,refresh]
+        },[page,refresh,prop.status]
     )
     const data:IState={
         current:studentList,refresh:()=>setRefresh(!refresh),update:(data:IPage)=>setPage(data)
