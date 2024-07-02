9730b8c61059b463f19bbbbb32357033ed979622
RUGINA <michel.igiraneza@auca.ac.rw>
2024-07-02 15:29:32 +0200

    add HospitalLicence control

--- src/page/hospital/licence/index.tsx
+++ src/page/hospital/licence/index.tsx
@@ -1,8 +1,10 @@
 import { Navigation } from "../../../component/navigation"
 import { HospitalMenu } from "../../../utils/hospitalMenu"
+import { DisplayLicencedWorker } from "./crud/display"
 
 export const HospitalLicence=()=>{
     return <Navigation navItems={HospitalMenu}>
-        <div></div>
+        <div className="fs-3 fw-bolder mt-3">Licenced student</div>
+        <DisplayLicencedWorker/>
     </Navigation>
 }
